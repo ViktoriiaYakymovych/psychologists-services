c2ccc5db8bfade2d16b25640160e837fe586b275
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2024-04-10 16:37:35 +0300

    i

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
-import Auth from "../Auth/Auth";
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("../../pages/Home/Home"));
+const Psychologists = lazy(() =>
+  import("../../pages/Psychologists/Psychologists")
+);
+const Favorites = lazy(() => import("../../pages/Favorites/Favorites"));
 
 function App() {
   return (
-    <>
-      <Auth />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/psychologists" element={<Psychologists />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Auth from "../../components/Auth/Auth";
+
+const Home = () => {
+  return (
+    <>
+      <p>Home</p>
+      <Auth />{" "}
+    </>
+  );
+};
+
+export default Home;
